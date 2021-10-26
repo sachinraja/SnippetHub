dd5107a6124630a9aedfbcfa041ac30bf71119e0
Sachin Raja <sachinraja2349@gmail.com>
2021-10-26 23:27:57 +0000

    fix: nexus always generate artifacts

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,15 +1,12 @@
 import path from 'path'
 import { applyMiddleware } from 'graphql-middleware'
 import { makeSchema } from 'nexus'
-// this is a problem with the nexus-prisma package,
-// not sure why it's erroring, but it does work
-// eslint-disable-next-line import/no-unresolved
 import NexusPrismaScalars from 'nexus-prisma/scalars'
 import * as types from './objects'
 import permissions from './permissions'
 
 const dirPath = path.join(process.cwd(), 'src', 'graphql')
-console.log(process.cwd(), dirPath)
+
 const schema = makeSchema({
   contextType: {
     module: path.join(dirPath, 'context.ts'),
@@ -19,6 +16,8 @@ const schema = makeSchema({
     schema: path.join(dirPath, '__generated__', 'schema.graphqls'),
     typegen: path.join(dirPath, '__generated__', 'nexus.d.ts'),
   },
+  // always generate, even in prod (graphql-let requires it)
+  shouldGenerateArtifacts: true,
   types: [types, NexusPrismaScalars],
 })
 
